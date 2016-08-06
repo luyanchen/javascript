eda1c80b8411a96bb0393a48c042bbbfb3c0a6e6
chenluyan <chenluyan_bupt@163.com>
2016-08-06 22:43:02 +0800

    20160806

--- test.js
+++ test.js
@@ -1,37 +1,45 @@
-process.stdin.resume();
-process.stdin.setEncoding('ascii');
-
-var input = "";
-var input_array = "";
-
-process.stdin.on('data', function (data) {
-    input += data;
-});
-
-process.stdin.on('end', function () {
-    input_array = input.split("\n");
-    var nLine = 0;
-
-    while(nLine < input_array.length){
-        var line = input_array[nLine++].trim();
-        if(line === ''){
-            continue;
-        }
-        var input_arrays = line.split(' ');
-        console.log(input_arrays);
-        for(var t=0;t<input_arrays.length;t++){
-            var s = +input_arrays[i];
-            var count=0;
-            for(var i=1;i<=s;i++){
-                for(var j=1;j<=s;j++){
-                    if(i*i+j*j == s){
-                        count ++;
-                    }
+var a = "001110";
+        var t,i,j=0;
+        var leftnum,rightnum=0;
+        var result;
+        var left = "";
+        var right = "";
+        if(a.indexOf("01") > 0){
+            t = a.indexOf("01");
+            left = a.slice(0,t+2);
+            right = a.slice(t+2);
+            i=t;j=t+1;
+            leftnum=0;
+            rightnum = 0;
+            while(left.length>0){
+                while(i>=0){
+                   if(left[i] == 0){
+                     leftnum++;
+                     i--;
+                   }
                 }
+                if(leftnum < (t+1)/2){
+                     left = left.slice(1);
+                }else{
+                    break;
+                }              
             }
-            console.log(2*count);
+            while(right.length>0){
+                while(j<a.length){
+                   if(right[j] == 0){
+                     rightnum++;
+                     j++;
+                   }
+                }
+                if(rightnum < (a.length-t-1)/2){
+                     right = right.slice(t+1,a.length-1);
+                }else{
+                    break;
+                }              
+            }
+            if(left.length>0 && right.length>0){
+                result.push(left.length+right.length);
+            }
+            a = a.slice(t+2);
+           
         }
-        
-        
-    }
-});
